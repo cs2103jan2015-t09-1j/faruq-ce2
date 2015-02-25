784e8a370108e12bc120598f7e10b9d368ace4dc
Faruq Rasid <me@ruqqq.sg>
2015-02-25 21:53:33 +0800

    Added more comments to describe architecture.

--- src/TextBuddy.java
+++ src/TextBuddy.java
@@ -19,6 +19,13 @@ import java.util.Scanner;
 //
 // According to user input, add, clear, delete, display data to/from file.
 //
+// Summarized Architecture:
+// 		User -> UI -> Logic (Controller + Command + Data) -> Storage
+//
+// Code-wise the flow is: main() -> Controller/Command -> UI / Data
+//
+// TLDR; Controller (including Command class) controls the flow of the program.
+//
 public class TextBuddy {
 	public static void main(String[] args) {
 		// Hand over to our controller
